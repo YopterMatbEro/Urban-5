ea4f2f65e8e2f15c9bd5744c851d21a240416cc6
YopterMatbEro <yopter2013@gmail.com>
2024-08-24 18:54:04 +0300

    arithmetic methods

--- module_5_1.py
+++ module_5_1.py
@@ -3,17 +3,147 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __len__(self):
+        if isinstance(self.number_of_floors, int):
+            return self.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __eq__(self, other):
+        if isinstance(self.number_of_floors, int) and isinstance(other, object):
+            return self.number_of_floors == other
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __ne__(self, other):
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors != other.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __lt__(self, other):
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors < other.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __le__(self, other):
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors <= other.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __gt__(self, other):
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors > other.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __ge__(self, other):
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors >= other.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __add__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors += value
+            return self.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __sub__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors -= value
+            return self.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __rsub__(self, value):
+        return self.__sub__(value)
+
+    def __isub__(self, value):
+        return self.__sub__(value)
+
+    def __mul__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors *= value
+            return self.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __rmul__(self, value):
+        return self.__mul__(value)
+
+    def __imul__(self, value):
+        return self.__mul__(value)
+
+    def __truediv__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors /= value
+            return self.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __rtruediv__(self, value):
+        return self.__truediv__(value)
+
+    def __itruediv__(self, value):
+        return self.__truediv__(value)
+
+    def __floordiv__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors //= value
+            return self.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __rfloordiv__(self, value):
+        return self.__rfloordiv__(value)
+
+    def __ifloordiv__(self, value):
+        return self.__rfloordiv__(value)
+
+    def __mod__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors %= value
+            return self.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __rmod__(self, value):
+        return self.__mod__(value)
+
+    def __imod__(self, value):
+        return self.__mod__(value)
+
+    def __pow__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors **= value
+            return self.number_of_floors
+        else:
+            raise TypeError('Кол-во этажей должно быть целым числом')
+
+    def __rpow__(self, value):
+        return self.__pow__(value)
+
+    def __ipow__(self, value):
+        return self.__pow__(value)
+
     def goto(self, new_floor: int):
         if new_floor > self.number_of_floors or new_floor < 1:
             print(f"В этом доме нет этажа {new_floor}")
         else:
             [print(i) for i in range(1, new_floor + 1)]
-
-    def __len__(self):
-        return self.number_of_floors
-
-    def __str__(self):
-        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
 
 if __name__ == '__main__':
