022bc8fae0369682accaf7daf9403e07955c2a69
YopterMatbEro <yopter2013@gmail.com>
2024-08-24 17:08:05 +0300

    initial commit

---
+++ module_5_1.py
@@ -0,0 +1,19 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def goto(self, new_floor: int):
+        print()
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print(f"В этом доме нет этажа {new_floor}", end=' ')
+        else:
+            [print(i, end=' ') for i in range(1, new_floor + 1)]
+
+
+lastochka = House("ЖК Ласточка", 5)
+
+lastochka.goto(5)
+lastochka.goto(10)
+lastochka.goto(4)
+lastochka.goto(0)
