117b74111f3005409fa5b2b514f3a3b3488c8180
YopterMatbEro <yopter2013@gmail.com>
2024-08-26 18:56:30 +0300

    add magical methods __new__, __del__

--- module_5_1.py
+++ module_5_1.py
@@ -1,10 +1,20 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        # obj = super().__new__(cls)
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
 
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
 
     def __len__(self):
         if isinstance(self.number_of_floors, int):
